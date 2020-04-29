96b885c714365f7d6cd9eac3578185ae68d3e878
Marius Schmitt <mariusschmitt9999@gmx.de>
2020-04-29 14:25:29 +0200

    Average staffline width and whitespace calculation implemented

--- Musicus/src/start/Start.java
+++ Musicus/src/start/Start.java
@@ -12,8 +12,10 @@ import general.Color;
 import general.Staffline;
 import interfaces.Binarization;
 import interfaces.StafflineDetection;
+import interfaces.StafflineRemoval;
 import interfaces.SystemDetection;
 import stafflinedetection.ProjectionStafflineDetection;
+import stafflineremoval.ClarkeStafflineRemoval;
 import systemdetection.FloodfillSystemDetection;
 import utils.ImageConverter;
 
@@ -85,14 +87,65 @@ public class Start implements Runnable{
 			}
 		}
 		
+		
 		//STAFFLINE DETECTION
 		double staffline_threshold = 0.7;
 		StafflineDetection stafflineDetection = new ProjectionStafflineDetection(staffline_threshold);
-		ArrayList<Staffline> stafflines;
+		ArrayList<ArrayList<Staffline>> stafflinesOfSystems = new ArrayList<>();
 		
 		for(boolean[][] system : systems) {
-			stafflines = stafflineDetection.detectStafflines(system);
+			stafflinesOfSystems.add(stafflineDetection.detectStafflines(system));
 		}
+		
+		
+		//Calculate Avg. Whitespace and Avg. Linethickness
+		//Assuming we only have 5 stafflines per staff
+		int numberOfStafflinesPerStaff = 5;
+		double avgWhitespace = 0;
+		double avgLineWidth = 0;
+		int countWhite = 0;
+		int countLine = 0;
+
+		for (int i = 0; i < stafflinesOfSystems.size(); i++) {
+			Staffline previous = null;
+			Staffline current = null;
+			
+			for (int j = 0; j < stafflinesOfSystems.get(i).size(); j++) {
+				current = stafflinesOfSystems.get(i).get(j);
+				
+				if(j % numberOfStafflinesPerStaff == 0) {
+					previous = current;
+				}
+				else {
+					//Distance between current line topmost and previous line bottommost for start and endpoint.
+					double whitespaceStart = Math.abs((previous.getStartPoint().getY() + (previous.getWidth() - 1)) - current.getStartPoint().getY() - 1);
+					double whitespaceEnd = Math.abs((previous.getEndPoint().getY() + (previous.getWidth() - 1)) - current.getEndPoint().getY() - 1);
+					avgWhitespace +=  (whitespaceStart + whitespaceEnd) / 2;
+					
+					countWhite++;
+					previous = current;
+				}
+				
+				avgLineWidth += current.getWidth();
+				countLine++;
+				
+			}
+		}
+		
+		avgLineWidth /= countLine;
+		avgWhitespace /= countWhite;
+			
+		if(Globals.DEBUG) {
+			System.out.println("Average line width: " + avgLineWidth);
+			System.out.println("Average whitespace: " + avgWhitespace);
+		}
+		 
+		//STAFFLINE REMOVAL
+		StafflineRemoval stafflineRemoval = new ClarkeStafflineRemoval();
+		for(int x = 0; x < systems.size() && x < stafflinesOfSystems.size(); x++) {
+			stafflineRemoval.removeStafflines(systems.get(x), stafflinesOfSystems.get(x));
+		}
+		
 	}
 	
 	
